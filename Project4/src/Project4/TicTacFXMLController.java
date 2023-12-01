cb22c1f4cf36bbd52f00cf42f758a255965139b6
Untraditional <58677398+Untraditional@users.noreply.github.com>
2023-12-01 14:26:59 -0500

    Throwing around ideas. Not complete at all

--- Project4/src/Project4/TicTacFXMLController.java
+++ Project4/src/Project4/TicTacFXMLController.java
@@ -6,21 +6,40 @@ package Project4;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
-/**
- * FXML Controller class
- *
- * @author Alex
- */
+
 public class TicTacFXMLController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private Button m00, m01, m02, m10, m11, m12, m20, m21, m22;
+    
+    @FXML
+    private Label lblWinner;
+
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    public void initialize(URL url, ResourceBundle rb) {}    
+
+    @FXML
+    private void handleCLick(ActionEvent event) {
+        if (m00.getText().equals("")) {
+            m00.setText("X");
+        } 
+        else if (m01.getText().equals("")) {
+            m01.setText("O");
+        }
+        else if (m02.getText().equals("") ) {
+            m02.setText("O");
+        }
+        
+    }
+    
+    private boolean winnerCheck(){
+        return true;
+    }
     
 }
--- Project4/src/Project4/server.java
+++ Project4/src/Project4/server.java
@@ -1,7 +1,50 @@
 
 package Project4;
 
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 public class server {
-    
+    private ServerSocket serverSocket;
+    private final int PORT = 8000; // Port number for the server
+    private Socket clientSocket;
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
+
+    public server() {
+        try {
+            serverSocket = new ServerSocket(PORT);
+            System.out.println("Server started on port " + PORT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void listen() {
+        while (true) {
+            try {
+                clientSocket = serverSocket.accept();
+                out = new ObjectOutputStream(clientSocket.getOutputStream());
+                in = new ObjectInputStream(clientSocket.getInputStream());
+
+                // Handle game logic here
+                // e.g., read client's move, update game state, send response
+
+            } catch (IOException e) {
+            } finally {
+                try {
+                    clientSocket.close();
+                    in.close();
+                    out.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        server server = new server();
+        server.listen();
+    }
 }
